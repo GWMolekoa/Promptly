254b2a10fa5d509924e19482a336f8f31fb6b071
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-11-07 11:21:49 +0200

    Add Google Auth environment variables

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+// Setup providers - Google Authentication
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    async session({ session }) {
+
+    },
+    async signIn({ profile }) {
+
+    }
+})
+
+export {handler as GET, handler as POST};
