f2bf485f615e1db9c57b292396f06cf29a27c214
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-11-25 17:13:28 +0200

    Modified Feed.jsx To have id and name attribute in labels

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
@@ -14,24 +14,23 @@ const PromptCardList = ({ data, handleTagClick }) => {
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
-
-  }
+    setSearchText(e.target.value); // Add functionality to handle search
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch('/api/prompt');
       const data = await response.json();
-
       setPosts(data);
-    }
+    };
 
     fetchPosts();
   }, []);
@@ -41,6 +40,8 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input 
           type="text"
+          id="search" // Added id
+          name="search" // Added name
           placeholder="Search For A Tag or Username."
           value={searchText}
           onChange={handleSearchChange}
@@ -52,11 +53,9 @@ const Feed = () => {
       <PromptCardList 
         data={posts}
         handleTagClick={() => {}}
-      
       />
-      
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
